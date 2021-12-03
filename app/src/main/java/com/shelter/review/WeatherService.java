be4cf2c1ad0bf555ba73e9e76580e192c89c35fc
xietian <xietian@021.com>
2021-12-03 17:21:15 +0800

    使用ProxyGenerator生成的动态代理类的class

---
+++ app/src/main/java/com/shelter/review/WeatherService.java
@@ -0,0 +1,12 @@
+package com.shelter.review;
+
+/**
+ * @author: Shelter
+ * Create time: 2021/12/3, 17:04.
+ */
+public interface WeatherService {
+
+    String getWeather();
+
+    void postWeather();
+} 
